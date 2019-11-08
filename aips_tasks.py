a0296b093971d1ac3b1fa3f876b84ce255d70492
abaczko <57004647+abaczko@users.noreply.github.com>
2019-11-08 15:52:19 +0100

    Update aips_tasks.py

--- aips_tasks.py
+++ aips_tasks.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env ParselTongue
+
+###########################################################################
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###########################################################################
+# Original author: Anne-Kathrin Baczko (MPIfR) baczko(AT)mpifr-bonn.mpg.de
+###########################################################################
+
 #
 '''
 This module contains several functions to pass commands to AIPS via ParselTongue.
